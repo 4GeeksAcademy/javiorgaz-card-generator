afae75deb1ccd28973bf3cfcff4ba92160126c14
javier <javi.orgaz00@gmail.com>
2024-11-15 16:04:11 +0100

    creado el repo

--- src/app.js
+++ src/app.js
@@ -2,10 +2,46 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const suits = ["spade", "heart", "diamond", "club"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const suitSimbols = {
+  spade: "♠",
+  diamond: "♦",
+  heart: "♥",
+  club: "♣"
 };
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getCardNumber() {
+  const n = getRandomInt(1, 9);
+  return n;
+}
+
+function getCard() {
+  const suitIndex = getRandomInt(0, suits.length - 1);
+  return suits[suitIndex];
+}
+
+function generateCard() {
+  //write your code here
+
+  const card = getCard();
+  const number = getCardNumber();
+
+  document.querySelector(".top-side").textContent = suitSimbols[card];
+  document.querySelector(".cardNumber").textContent = number;
+  document.querySelector(".bottom-side").textContent = suitSimbols[card];
+
+  if (card === "heart" || card === "diamond") {
+    document.querySelector(".card").style.color = "red";
+  } else {
+    document.querySelector(".card").style.color = "black";
+  }
+}
+
+window.onload = generateCard;
+
+document.getElementById("reLoad").onclick = generateCard;
